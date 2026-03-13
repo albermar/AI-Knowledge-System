475fe4d0cec03f1b327fcebee0cf834b92fc144e
Alberto Bermejillo Martín-Romo <alberto.bmoi@gmail.com>
2026-03-13 17:26:20 +0100

    deploying tests

--- app/api/main.py
+++ app/api/main.py
@@ -1,12 +1,16 @@
 from fastapi import FastAPI
-from app.api import router_1_ingest_document, router_3_ask_question, router_2_add_organization, router_4_dashboard
-
-from dotenv import load_dotenv
-load_dotenv()  # Load environment variables from .env file
+#from app.api import router_1_ingest_document, router_3_ask_question, router_2_add_organization, router_4_dashboard
 
 app = FastAPI(title = "AI Knowledge System API", version = "1.0")
 
+@app.get("/")
+def root():
+    return {"status": "ok"}
+
+'''
 app.include_router(router_1_ingest_document.router, prefix = "/api", tags = ["ingest_document"])
 app.include_router(router_2_add_organization.router, prefix = "/api", tags = ["new_organization"])
 app.include_router(router_3_ask_question.router, prefix = "/api", tags = ["ask_question"])
 app.include_router(router_4_dashboard.router, prefix="/api", tags=["dashboard"])
+
+'''
